b24793643846cf20f639d0436d4a32fe3c4774bc
angulu <angulumbi@middlebury.edu>
2020-06-18 07:51:59 -0400

    Done with controller and model

---
+++ FeastFreedom/FeastFreedom/Controllers/KitchensController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FeastFreedom.Models;
+
+namespace FeastFreedom.Controllers
+{
+    public class KitchensController : Controller
+    {
+        private feastfreedomEntities db = new feastfreedomEntities();
+
+        // GET: Kitchens
+        public ActionResult Index()
+        {
+            return View(db.Kitchens.ToList());
+        }
+
+        // GET: Kitchens/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kitchen kitchen = db.Kitchens.Find(id);
+            if (kitchen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kitchen);
+        }
+
+        // GET: Kitchens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Kitchens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KitchenId,KitchenName,UserId,WorkingDays,StartTime,CloseTime,Image")] Kitchen kitchen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kitchens.Add(kitchen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kitchen);
+        }
+
+        // GET: Kitchens/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kitchen kitchen = db.Kitchens.Find(id);
+            if (kitchen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kitchen);
+        }
+
+        // POST: Kitchens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KitchenId,KitchenName,UserId,WorkingDays,StartTime,CloseTime,Image")] Kitchen kitchen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kitchen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kitchen);
+        }
+
+        // GET: Kitchens/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kitchen kitchen = db.Kitchens.Find(id);
+            if (kitchen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kitchen);
+        }
+
+        // POST: Kitchens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kitchen kitchen = db.Kitchens.Find(id);
+            db.Kitchens.Remove(kitchen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
